bc6f95cfebfc9f895eb907a84a44139cefe320b5
Evgeny Barabanov <meltedspark@gmail.com>
2018-07-06 15:34:00 +0300

    chore: fix mistypes

--- e2e/custom-webpack-browser-schema.spec.ts
+++ e2e/custom-webpack-browser-schema.spec.ts
@@ -3,11 +3,11 @@ describe('custom webpack browser builder test', () => {
 
 	beforeEach(() => {
 		jest.resetModules()
-		customWebpackBrowserSchema = require('../src/custom-webpack/server/schema.json');
+		customWebpackBrowserSchema = require('../src/custom-webpack/browser/schema.json');
 	});
 
-	it('Should fit the schema of @angular-devkit/build-angular:server', () => {
-		const originalBrowserSchema = require('@angular-devkit/build-angular/src/server/schema.json');
+	it('Should fit the schema of @angular-devkit/build-angular:browser', () => {
+		const originalBrowserSchema = require('@angular-devkit/build-angular/src/browser/schema.json');
 		customWebpackBrowserSchema.properties['webpackConfigPath'] = undefined;
 		expect(originalBrowserSchema.properties).toEqual(customWebpackBrowserSchema.properties);
 	});
--- e2e/custom-webpack-server-schema.spec.ts
+++ e2e/custom-webpack-server-schema.spec.ts
@@ -1,13 +1,13 @@
-describe('custom webpack browser builder test', () => {
+describe('custom webpack server builder test', () => {
 	let customWebpackBrowserSchema: any;
 
 	beforeEach(() => {
 		jest.resetModules()
-		customWebpackBrowserSchema = require('../src/custom-webpack/browser/schema.json');
+		customWebpackBrowserSchema = require('../src/custom-webpack/server/schema.json');
 	});
 
-	it('Should fit the schema of @angular-devkit/build-angular:browser', () => {
-		const originalBrowserSchema = require('@angular-devkit/build-angular/src/browser/schema.json');
+	it('Should fit the schema of @angular-devkit/build-angular:server', () => {
+		const originalBrowserSchema = require('@angular-devkit/build-angular/src/server/schema.json');
 		customWebpackBrowserSchema.properties['webpackConfigPath'] = undefined;
 		expect(originalBrowserSchema.properties).toEqual(customWebpackBrowserSchema.properties);
 	});
