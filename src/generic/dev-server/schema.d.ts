aa589c5cb54591448baaf570d5c50a25ab426f56
Scott Phillips <scott.phillips@sailpoint.com>
2018-07-24 10:22:43 -0500

    Cleanup

--- src/generic/dev-server/schema.d.ts
+++ src/generic/dev-server/schema.d.ts
@@ -1,7 +1,3 @@
-/**
- * Created by Evgeny Barabanov on 01/07/2018.
- */
-
 import { BrowserBuilderSchema } from '@angular-devkit/build-angular';
 
 export interface GenericDevServerBuildSchema extends BrowserBuilderSchema { }
