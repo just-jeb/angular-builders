fc085b6bc43fe09520ae8213c02997caeef98330
Evgeny Barabanov <meltedspark@gmail.com>
2018-08-01 23:32:41 +0300

    chore: move all to .ts

--- default-config.ts
+++ default-config.ts
@@ -1,6 +1,6 @@
 export default {
   'preset': 'jest-preset-angular',
-  'testURL': 'https://github.com/meltedspark/angular-cli-builders',
+  'testURL': 'https://github.com/@angular-cli-builders',
   'setupTestFrameworkScriptFile': `${__dirname}/setup.js`,
   "moduleNameMapper": {
     "\\.(jpg|jpeg|png)$": `${__dirname}/mock-module.js`,
