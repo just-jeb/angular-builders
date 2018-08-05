886c5ca8daee1b8e5aa0ef4fdefa77f96f1a4fe2
Evgeny Barabanov <meltedspark@gmail.com>
2018-08-05 17:49:12 +0300

    Handle error

--- src/timestamp.builder.ts
+++ src/timestamp.builder.ts
@@ -1,6 +1,6 @@
 import {Builder, BuilderConfiguration, BuilderContext, BuildEvent} from '@angular-devkit/architect';
-import {bindNodeCallback, Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {bindNodeCallback, Observable, of} from 'rxjs';
+import {catchError, map, tap} from 'rxjs/operators';
 import {TimestampBuilderSchema} from './schema';
 import {getSystemPath} from '@angular-devkit/core';
 import {writeFile} from 'fs';
@@ -15,7 +15,14 @@ export default class TimestampBuilder implements Builder<TimestampBuilderSchema>
     const {path, format} = builderConfig.options;
     const timestampFileName = `${getSystemPath(root)}/${path}`;
     const writeFileObservable = bindNodeCallback(writeFile);
-    return writeFileObservable(timestampFileName, dateFormat(new Date(), format)).pipe(map(() => ({success: true})));
+    return writeFileObservable(timestampFileName, dateFormat(new Date(), format)).pipe(
+      map(() => ({success: true})),
+      tap(() => this.context.logger.info("Timestamp created")),
+      catchError(e => {
+        this.context.logger.error("Failed to create timestamp", e);
+        return of({success: false});
+      })
+    );
   }
 
 }
