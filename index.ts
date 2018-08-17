e3863906d1ea21cba1d6155b77bb8275c8a9f9af
Evgeny Barabanov <meltedspark@gmail.com>
2018-08-17 19:13:55 +0300

    chore: start moving to monorepo

--- index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Evgeny Barabanov on 01/07/2018.
- */
-export * from './src/custom-webpack/browser/index';
-export * from './src/custom-webpack/server/index';
-export * from './src/generic/dev-server/index';
