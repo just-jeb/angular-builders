04050e7772ddea32de6b933a7ad281ec2a14d90a
Evgeny Barabanov <meltedspark@gmail.com>
2018-08-18 14:33:05 +0300

    fix: make custom-webpack compilation, ut and e2e pass

--- packages/generic/e2e/generic-dev-server-schema.spec.ts
+++ packages/generic/e2e/generic-dev-server-schema.spec.ts
@@ -3,7 +3,7 @@ describe('generic dev server builder test', () => {
 
 	beforeEach(() => {
 		jest.resetModules()
-		genericDevServerSchema = require('../packages/generic/dev-server/schema.json');
+		genericDevServerSchema = require('../dev-server/schema.json');
 	});
 
 	it('Should fit the schema of @angular-devkit/build-angular:dev-server', () => {
