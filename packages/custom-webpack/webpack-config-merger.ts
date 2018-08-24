e8645aabb47a49d942962272fda11a0ca87b52c6
Evgeny Barabanov <meltedspark@gmail.com>
2018-08-24 17:55:56 +0300

    feat(custom-webpack): support loaders merge

--- packages/custom-webpack/webpack-config-merger.ts
+++ packages/custom-webpack/webpack-config-merger.ts
@@ -1,56 +1,15 @@
 import {MergeStrategies} from "./custom-webpack-builder-config";
 import * as webpackMerge from 'webpack-merge';
 import {Configuration} from "webpack";
-import {mergeWith, differenceWith} from 'lodash';
-import {CustomizeArrayFunction} from "webpack-merge";
-
-const joinArrays = require('webpack-merge/lib/join-arrays');
+import {differenceWith} from 'lodash';
 
 export class WebpackConfigMerger {
 	static merge(webpackConfig1: Configuration, webpackConfig2: Configuration, mergeStrategies: MergeStrategies = {}, replacePlugins = false): Configuration {
-		return webpackMerge({
-			customizeArray: replacePlugins ? mergeReplacePlugins(customizeArray(mergeStrategies)) : customizeArray(mergeStrategies),
-			customizeObject: customizeObject(mergeStrategies)
-		})
-		(webpackConfig1, webpackConfig2)
+		const mergedConfig = webpackMerge.smartStrategy(mergeStrategies)(webpackConfig1, webpackConfig2);
+		if(webpackConfig1.plugins && webpackConfig2.plugins && replacePlugins) {
+      const aExceptb = differenceWith(webpackConfig1.plugins, webpackConfig2.plugins, (item1, item2) => item1.constructor.name === item2.constructor.name);
+      mergedConfig.plugins = [...aExceptb, ...webpackConfig2.plugins];
+    }
+		return mergedConfig;
 	}
-}
-
-function mergeReplacePlugins(defaultArrayMerger: CustomizeArrayFunction) {
-	return (a: any[], b: any[], key: string) => {
-		let plugins;
-		if (key === 'plugins') {
-			const aExceptb = differenceWith(a, b, (item1: Plugin, item2: Plugin) => item1.constructor.name === item2.constructor.name);
-			plugins = [...aExceptb, ...b];
-		} else {
-			plugins = defaultArrayMerger(a, b, key);
-		}
-		return plugins;
-	}
-}
-
-function customizeArray(strategies: MergeStrategies) {
-	return (a: any[], b: any[], key: string) => {
-		switch (strategies[key]) {
-			case 'prepend':
-				return [...b, ...a];
-			case 'replace':
-				return b;
-			default: // append
-				return undefined;
-		}
-	};
-}
-
-function customizeObject(strategies: MergeStrategies) {
-	return (a: any, b: any, key: string) => {
-		switch (strategies[key]) {
-			case 'prepend':
-				return mergeWith({}, b, a, joinArrays());
-			case 'replace':
-				return b;
-			default: // append
-				return undefined;
-		}
-	};
 }
