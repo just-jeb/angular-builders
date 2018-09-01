9717586e6ee58d00f412c3741c5f5659bee53cc8
Evgeny Barabanov <meltedspark@gmail.com>
2018-09-01 15:30:09 +0300

    fix: remove jest builder before installing from local

--- packages/jest/scripts/ci.sh
+++ packages/jest/scripts/ci.sh
@@ -2,9 +2,9 @@
 filename=jest-builder.tgz
 
 yarn pack --filename ${filename}
-cd ./examples/simple-app && yarn add -D file:../../${filename}
+cd ./examples/simple-app && yarn remove @angular-builders/jest && yarn add -D file:../../${filename}
 yarn test
 yarn e2e -- --protractor-config=./e2e/protractor-ci.conf.js
-cd ../multiple-apps && yarn add -D file:../../${filename}
+cd ../multiple-apps && yarn remove @angular-builders/jest && yarn add -D file:../../${filename}
 yarn run test my-first-app && yarn run test my-second-app && yarn run test my-shared-library
 yarn run e2e my-first-app-e2e -- --protractor-config=./projects/my-first-app-e2e/protractor-ci.conf.js && yarn run e2e my-second-app-e2e -- --protractor-config=./projects/my-second-app-e2e/protractor-ci.conf.js
