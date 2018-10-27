378d30ca5078b813dcaf401ad79ce16b223e9caf
Evgeny Barabanov <meltedspark@gmail.com>
2018-10-27 16:44:04 +0300

    fix: replace Error properly

--- jest-custom-environment.js
+++ jest-custom-environment.js
@@ -1,15 +1,30 @@
 const NodeEnvironment = require('jest-environment-node');
 
-// Return array of CallSites instead of string
-// callsites package does the same, but as we are replacing the original Error with Jest's Error,
-// callsites' replacement won't affect the original V8 Error
-Error.prepareStackTrace = (_, stack) => stack;
+
+const originalPrepareStackTrace = Error.prepareStackTrace;
+const originalError = Error;
+
 
 class JestCustomEnvironment extends NodeEnvironment {
     constructor(config){
         super(config);
+
+    }
+
+    async setup(){
+        await super.setup();
+        // Return array of CallSites instead of string
+        // callsites package does the same, but as we are replacing the original Error with Jest's Error,
+        // callsites' replacement won't affect the original V8 Error
+        Error.prepareStackTrace = (_, stack) => stack;
         // Workaround for this bug https://github.com/facebook/jest/issues/2549
         Error = this.global.Error;
+    }
+
+    async tearDown(){
+        Error = originalError;
+        Error.prepareStackTrace = originalPrepareStackTrace;
+       await super.tearDown();
     }
 }
 
