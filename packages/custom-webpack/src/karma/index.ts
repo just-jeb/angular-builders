3da17a100c70b1e1d4cf325865427ad3ae28c1cb
Evgeny Barabanov <meltedspark@gmail.com>
2018-12-04 17:31:50 +0200

    chore: update dependencies

--- packages/custom-webpack/src/karma/index.ts
+++ packages/custom-webpack/src/karma/index.ts
@@ -3,26 +3,28 @@
  */
 
 import {BuilderContext} from '@angular-devkit/architect';
-import {KarmaBuilder} from '@angular-devkit/build-angular';
+import {KarmaBuilder, NormalizedKarmaBuilderSchema} from '@angular-devkit/build-angular';
 import {Path, virtualFs} from '@angular-devkit/core';
 import * as fs from 'fs';
-import {NormalizedCustomWebpackBrowserBuildSchema} from "../browser";
-import {CustomWebpackBuilder} from "../custom-webpack-builder";
-import {Configuration} from "webpack";
+import {CustomWebpackBuilder} from '../custom-webpack-builder';
+import {Configuration} from 'webpack';
+import {CustomWebpackSchema} from '../custom-webpack-schema';
+
+export interface NormalizedCustomWebpackKarmaBuildSchema extends NormalizedKarmaBuilderSchema, CustomWebpackSchema {
+}
 
 export class CustomWebpackKarmaBuilder extends KarmaBuilder {
 
   constructor(context: BuilderContext) {
     super(context);
-    super['_buildWebpackConfig'] = this.buildWebpackConfig;
   }
 
   buildWebpackConfig(root: Path,
                      projectRoot: Path,
                      sourceRoot: Path,
                      host: virtualFs.Host<fs.Stats>,
-                     options: NormalizedCustomWebpackBrowserBuildSchema): Configuration {
-    const karmaConfig = KarmaBuilder.prototype['_buildWebpackConfig'].call(this, root, projectRoot, sourceRoot, host, options);
+                     options: NormalizedCustomWebpackKarmaBuildSchema): Configuration {
+    const karmaConfig = super.buildWebpackConfig(root, projectRoot, sourceRoot, host, options);
 	  return CustomWebpackBuilder.buildWebpackConfig(root, options.customWebpackConfig, karmaConfig);
   }
 }
