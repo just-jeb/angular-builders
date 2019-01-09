87c48a96cee3901b4ca56e3b54d467cdc76c65aa
Evgeny Barabanov <meltedspark@gmail.com>
2019-01-09 15:05:22 +0200

    fix(dev-server-generic): fix builder schema typings

--- packages/dev-server/src/generic/index.ts
+++ packages/dev-server/src/generic/index.ts
@@ -40,7 +40,7 @@ export class GenericDevServerBuilder extends DevServerBuilder {
     root: Path,
     projectRoot: Path,
     host: virtualFs.Host<Stats>,
-    browserOptions: BrowserBuilderSchema,
+    browserOptions: NormalizedBrowserBuilderSchema,
   ): Configuration {
     // Check if we can use the generic webpack builder if so lets use it, otherwise we'll fall back to the DevServerBuilder's
     // implementation
