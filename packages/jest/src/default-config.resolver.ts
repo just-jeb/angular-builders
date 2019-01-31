85e103c573a51a512ff1e88f804d17d4bc3e7431
Evgeny Barabanov <meltedspark@gmail.com>
2019-01-31 15:55:27 +0200

    fix(jest): make global default configuration paths to take workspace root into account

--- packages/jest/src/default-config.resolver.ts
+++ packages/jest/src/default-config.resolver.ts
@@ -3,11 +3,25 @@ import defaultConfig from "./jest-config/default-config";
 
 export const testPattern = `/**/+(*.)+(spec|test).+(ts|js)?(x)`;
 export const tsConfigName = 'tsconfig.spec.json';
+export const jestPresetRootPath = `node_modules/jest-preset-angular`;
+export const preprocessor = 'preprocessor.js';
+export const snapshotSerializer = 'AngularSnapshotSerializer.js';
+export const htmlCommentSerializer = 'HTMLCommentSerializer.js';
+export const transformRegex = '^.+\\.(ts|js|html)$';
 
 export class DefaultConfigResolver {
 
-  resolveGlobal(): any {
-    return defaultConfig;
+  resolveGlobal(workspaceRoot: Path): any {
+    const jestPresetFullPath = join(workspaceRoot, jestPresetRootPath);
+    return {...defaultConfig,
+      transform: {
+       [transformRegex] : getSystemPath(join(jestPresetFullPath, preprocessor))
+      },
+      snapshotSerializers: [
+        getSystemPath(join(jestPresetFullPath, snapshotSerializer)),
+        getSystemPath(join(jestPresetFullPath, htmlCommentSerializer))
+      ]
+    };
   }
 
   resolveForProject(projectRoot: Path): any {
