c5dc9e0db491a2110cb3327135a202ca9cd3b216
Wes Grimes <wesgrimes@gmail.com>
2019-02-18 21:14:08 -0500

    feat(jest) replace deprecated setupTestFrameworkScriptFile with setupFilesAfterEnv

--- packages/jest/src/jest-config/default-config.ts
+++ packages/jest/src/jest-config/default-config.ts
@@ -4,7 +4,7 @@ export default {
   },
   preset: 'jest-preset-angular',
   testURL: 'https://github.com/@angular-cli-builders',
-  setupTestFrameworkScriptFile: `${__dirname}/setup.js`,
+  setupFilesAfterEnv: [`${__dirname}/setup.js`],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png)$': `${__dirname}/mock-module.js`
   }
