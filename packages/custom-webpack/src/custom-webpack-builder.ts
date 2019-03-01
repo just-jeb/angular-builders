faae33a9e9fd30ad4eb3172f4b5103b7bdc3528d
Chao Yang <chao.yang@fairfaxmedia.co.nz>
2019-03-01 14:19:43 +1300

    add a default value for customWebpackConfig and ignore the merging when it is not set

--- packages/custom-webpack/src/custom-webpack-builder.ts
+++ packages/custom-webpack/src/custom-webpack-builder.ts
@@ -7,6 +7,9 @@ export const defaultWebpackConfigPath = 'webpack.config.js';
 
 export class CustomWebpackBuilder {
 	static buildWebpackConfig(root: Path, config: CustomWebpackBuilderConfig, baseWebpackConfig: Configuration): Configuration{
+		if(!config) {
+			return baseWebpackConfig;
+		}
 		const webpackConfigPath = config.path || defaultWebpackConfigPath;
 		const customWebpackConfig = require(`${getSystemPath(root)}/${webpackConfigPath}`);
 		return WebpackConfigMerger.merge(baseWebpackConfig, customWebpackConfig, config.mergeStrategies, config.replaceDuplicatePlugins);
