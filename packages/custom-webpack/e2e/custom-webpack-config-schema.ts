fc8b3aae00b3eed15d898b27a32edb02deff6e37
Chao Yang <chaoyang.nz@gmail.com>
2019-03-02 11:04:29 +1300

    remove redundant default value

--- packages/custom-webpack/e2e/custom-webpack-config-schema.ts
+++ packages/custom-webpack/e2e/custom-webpack-config-schema.ts
@@ -5,7 +5,5 @@ export const customWebpackConfig = {
 		"path": {"type": "string", "description": "Path to the custom webpack configuration file"},
 		"mergeStrategies": {"type": "object", "description": "Merge strategies per webpack config field"},
 		"replaceDuplicatePlugins": {"type": "boolean", "description": "Flag that indicates whether to replace duplicate webpack plugins or not"}
-	},
-	// @ts-ignore
-	"default": null
+	}
 };
