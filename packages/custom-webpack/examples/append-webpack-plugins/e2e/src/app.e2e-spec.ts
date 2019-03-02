34c8d1f989bcf093b7848f2e301005349d109d34
Chao Yang <chaoyang.nz@gmail.com>
2019-03-02 20:31:53 +1300

    update e2e test description

--- packages/custom-webpack/examples/append-webpack-plugins/e2e/src/app.e2e-spec.ts
+++ packages/custom-webpack/examples/append-webpack-plugins/e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
-import {environment} from "../../src/environments/environment";
-import {by, element} from "protractor";
+import {environment} from '../../src/environments/environment';
+import {by, element} from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -9,7 +9,7 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display generated Hello World div', () => {
+  it('should display generated Hello World div when production build', () => {
     page.navigateTo();
     if(environment.production) {
       expect(page.getDivText()).toEqual('Hello world');
