e7cd23101de73a22e0fc08d05c22c811f7131799
Wes Grimes <wesgrimes@gmail.com>
2019-03-20 08:24:19 -0400

    incorporate changes from pr #269

--- packages/jest/src/jest-config/default-config.ts
+++ packages/jest/src/jest-config/default-config.ts
@@ -1,4 +1,10 @@
 export default {
+  globals: {
+    "ts-jest": {
+      stringifyContentPathRegex:  "\\.html$",
+      astTransformers: ['jest-preset-angular/InlineHtmlStripStylesTransformer.js']
+    }
+  },
   preset: 'jest-preset-angular',
   testURL: 'https://github.com/@angular-cli-builders',
   setupFilesAfterEnv: [`${__dirname}/setup.js`],
