0d8bcba0cd094950c58c333faa6556e2fd794c37
JeB <meltedspark@users.noreply.github.com>
2019-04-03 23:19:26 +0300

    Clean cache only when not in CI

--- packages/custom-webpack/scripts/ci.sh
+++ packages/custom-webpack/scripts/ci.sh
@@ -3,7 +3,9 @@
 filename=custom-webpack-builder.tgz
 set -e;
 yarn pack --filename ${filename}
-cd ./examples/append-webpack-plugins && yarn remove @angular-builders/custom-webpack && yarn cache clean @angular-builders/custom-webpack && yarn add -D file:../../${filename}
+cd ./examples/append-webpack-plugins && yarn remove @angular-builders/custom-webpack
+[ ! $CI ] && yarn cache clean
+yarn add -D file:../../${filename}
 
 cd ../sanity-app && yarn remove @angular-builders/custom-webpack && yarn cache clean @angular-builders/custom-webpack && yarn add -D file:../../${filename}
 
@@ -11,7 +13,10 @@ cd ../../../dev-server
 yarn build
 filename=dev-server-builder.tgz
 yarn pack --filename ${filename}
-cd ../custom-webpack/examples/append-webpack-plugins && yarn remove @angular-builders/dev-server && yarn cache clean @angular-builders/dev-server && yarn add -D file:../../../dev-server/${filename}
+cd ../custom-webpack/examples/append-webpack-plugins && yarn remove @angular-builders/dev-server
+[ ! $CI ] && yarn cache clean
+
+yarn add -D file:../../../dev-server/${filename}
 
 yarn e2e --protractor-config=./e2e/protractor-ci.conf.js
 yarn e2e --protractor-config=./e2e/protractor-ci.conf.js --prod
