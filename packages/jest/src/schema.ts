880a84d2b63ba211d81b11679cbee6b29dc8c924
adam <adam.jenkins@smartsti.com>
2019-04-06 15:28:46 -0300

    Accept numerical arguments and pass them to the CLI

--- packages/jest/src/schema.ts
+++ packages/jest/src/schema.ts
@@ -1,5 +1,5 @@
 export interface JestBuilderSchema {
   configPath: string;
   // Schema options are matching this: https://github.com/facebook/jest/blob/master/packages/jest-cli/src/cli/args.js
-  [option: string]: string | boolean | string[];
+  [option: string]: string | boolean | string[] | number;
 }
