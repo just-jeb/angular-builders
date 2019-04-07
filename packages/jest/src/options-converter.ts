3e0c402ca586b1172a72d4df86dd5d5f4d8686bf
Adam <akmjenkins@gmail.com>
2019-04-07 17:18:23 -0300

    Accept numerical arguments and pass them to the CLI (#292)

--- packages/jest/src/options-converter.ts
+++ packages/jest/src/options-converter.ts
@@ -7,7 +7,7 @@ export class OptionsConverter {
       let optionValue = options[option];
       if (optionValue === true) {
         argv.push(`--${option}`);
-      } else if (typeof optionValue === 'string') {
+      } else if (typeof optionValue === 'string' || typeof optionValue === 'number') {
         argv.push(`--${option}=${optionValue}`);
       } else if (Array.isArray(optionValue)) {
         for(const item of optionValue){
