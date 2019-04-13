5fe671bc5828b936d6a7c15e3652bb85eabc78c5
JeB <morethanjeb@gmail.com>
2019-04-13 21:54:31 +0300

    chore: update examples of custom-webpack builder

--- packages/custom-webpack/examples/append-webpack-plugins/src/app/app.component.spec.ts
+++ packages/custom-webpack/examples/append-webpack-plugins/src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('append-webpack-plugins');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
--- packages/custom-webpack/scripts/ci.sh
+++ packages/custom-webpack/scripts/ci.sh
@@ -4,15 +4,18 @@ filename=custom-webpack-builder.tgz
 set -e;
 yarn pack --filename ${filename}
 
-cd ./examples/append-webpack-plugins && yarn remove @angular-builders/custom-webpack
+cd ./examples/sanity-app && yarn remove @angular-builders/custom-webpack
 [ ! $CI ] && yarn cache clean
 yarn add -D file:../../${filename}
+yarn build
 
-cd ../sanity-app && yarn remove @angular-builders/custom-webpack
+cd ../append-webpack-plugins && yarn remove @angular-builders/custom-webpack
 [ ! $CI ] && yarn cache clean
 yarn add -D file:../../${filename}
+#check karma builder
+yarn test --browsers=ChromeHeadlessCI
+#check browser and dev-server
+yarn e2e --protractor-config=./e2e/protractor-ci.conf.js
+yarn e2e --protractor-config=./e2e/protractor-ci.conf.js --prod
 
-cd ../sanity-app && yarn build
 
-yarn new-e2e
-yarn new-e2e-prod
