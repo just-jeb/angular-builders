cbae3e415a13b7282dba950dc6a404e78c2782eb
patnolanireland <patnolanireland@gmail.com>
2019-05-11 20:49:53 +1000

    Added support for the @angular-devkit/build-angular:extract-i18n target

--- packages/custom-webpack/src/schemes.ts
+++ packages/custom-webpack/src/schemes.ts
@@ -13,5 +13,10 @@ module.exports = [
     originalSchemaPath: '@angular-devkit/build-angular/src/karma/schema.json',
     schemaExtensionPaths: [`${__dirname}/karma/schema.ext.json`, `${__dirname}/schema.ext.json`],
     newSchemaPath: `${__dirname}/karma/schema.json`
+  },
+	{
+    originalSchemaPath: '@angular-devkit/build-angular/src/extract-i18n/schema.json',
+    schemaExtensionPaths: [`${__dirname}/extract-i18n/schema.ext.json`, `${__dirname}/schema.ext.json`],
+    newSchemaPath: `${__dirname}/extract-i18n/schema.json`
   }
 ];
