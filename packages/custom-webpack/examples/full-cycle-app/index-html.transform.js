b397e5200428b9bd40d49eb9137aa9978659ffa6
JeB <morethanjeb@gmail.com>
2019-06-24 00:59:42 +0300

    chore: remove print

--- packages/custom-webpack/examples/full-cycle-app/index-html.transform.js
+++ packages/custom-webpack/examples/full-cycle-app/index-html.transform.js
@@ -1,5 +1,4 @@
 module.exports = (targetOptions, indexHtml) => {
-    console.log(targetOptions, indexHtml);
     const i = indexHtml.indexOf('</body>');
     const config = `<p>Configuration: ${targetOptions.configuration}</p>`
     return `${indexHtml.slice(0,i)}
