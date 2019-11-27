680b732ad09abe137efe6900e9a8c49cbf365438
JeB <9823087+just-jeb@users.noreply.github.com>
2019-11-27 13:42:04 +0200

    Update func-webpack.config.ts

--- packages/custom-webpack/examples/full-cycle-app/func-webpack.config.ts
+++ packages/custom-webpack/examples/full-cycle-app/func-webpack.config.ts
@@ -1,10 +1,11 @@
 import { Configuration } as webpack from 'webpack';
+import { CustomWebpackBrowserSchema } from '@angular-builders/custom-webpack';
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 
 /**
  * This is where you define a function that modifies your webpack config
  */
-export default (cfg: Configuration) => {
+export default (cfg: Configuration, opts: CustomWebpackBrowserSchema) => {
   cfg.plugins.push(
     new HtmlWebpackPlugin({
       filename: 'footer.html',
