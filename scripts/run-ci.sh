23f9235e2050e3377ebb7f7a749f0324de935236
JeB <morethanjeb@gmail.com>
2019-12-19 17:12:42 +0200

    ci: use verdaccio for e2e

---
+++ scripts/run-ci.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Start in scripts/ even if run from root directory
+cd "$(dirname "$0")"
+source local-registry.sh
+
+function cleanup {
+  echo 'Cleaning up.'
+  cd "$root_path"
+  # Uncomment when snapshot testing is enabled by default:
+  # rm ./packages/react-scripts/template/src/__snapshots__/App.test.js.snap
+  rm -rf "$temp_app_path"
+  # Restore the original NPM and Yarn registry URLs and stop Verdaccio
+  stopLocalRegistry
+}
+
+# Error messages are redirected to stderr
+function handle_error {
+  echo "$(basename $0): ERROR! An error was encountered executing line $1." 1>&2;
+  cleanup
+  echo 'Exiting with error.' 1>&2;
+  exit 1
+}
+
+function handle_exit {
+  cleanup
+  echo 'Exiting without error.' 1>&2;
+  exit
+}
+
+# Exit the script with a helpful error message when any error is encountered
+trap 'set +x; handle_error $LINENO $BASH_COMMAND' ERR
+
+# Cleanup before exit on any termination signal
+trap 'set +x; handle_exit' SIGQUIT SIGTERM SIGINT SIGKILL SIGHUP
+
+# Echo every command being executed
+set -x
+
+# Go to root
+cd ..
+root_path=$PWD
+
+startLocalRegistry "$root_path"/scripts/verdaccio.yaml
+
+publishToLocalRegistry
+
+yarn bootstrap:examples
+
+npx lerna run ci
+
+cleanup
