1a2b04345c9476a001a43ddea5cb26394b930273
JeB <9823087+just-jeb@users.noreply.github.com>
2019-12-24 00:07:20 +0200

    feat(jest)!: use jest-preset-angular@8.0.0

--- packages/jest/src/jest-config/default-config.ts
+++ packages/jest/src/jest-config/default-config.ts
@@ -1,10 +1,7 @@
+import {globals} from 'jest-preset-angular/jest-preset.js';
+
 export default {
-  globals: {
-    'ts-jest': {
-      stringifyContentPathRegex: '\\.html$',
-      astTransformers: ['jest-preset-angular/InlineHtmlStripStylesTransformer.js'],
-    },
-  },
+  globals,
   preset: 'jest-preset-angular',
   testURL: 'https://github.com/@angular-cli-builders',
   setupFilesAfterEnv: [`${__dirname}/setup.js`],
