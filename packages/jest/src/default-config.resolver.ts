0e04a6de6da9d2453a1933b07c3ad30d7101b32e
JeB <9823087+just-jeb@users.noreply.github.com>
2020-02-01 13:06:26 +0200

    fix(jest): improve testsMatch pattern for Jest 25 (#675)

--- packages/jest/src/default-config.resolver.ts
+++ packages/jest/src/default-config.resolver.ts
@@ -1,7 +1,7 @@
 import { getSystemPath, join, Path, normalize } from '@angular-devkit/core';
 import defaultConfig from './jest-config/default-config';
 
-export const testPattern = `/**/+(*.)+(spec|test).+(ts|js)?(x)`;
+export const testPattern = `/**/*(*.)@(spec|test).[tj]s?(x)`;
 export const tsConfigName = 'tsconfig.spec.json';
 
 export class DefaultConfigResolver {
