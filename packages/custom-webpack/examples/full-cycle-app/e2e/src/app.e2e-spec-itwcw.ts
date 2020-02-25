1c51a698343febf40641eec6ed6a24fbf6b67a49
vixnguyen <vinguyen.fitsgu@gmail.com>
2020-02-25 17:53:13 +0700

    Revert "adding text to debug"
    This reverts commit 3f2d0ede5fb171ec14970c3f9a2463c0a72eb158.

--- packages/custom-webpack/examples/full-cycle-app/e2e/src/app.e2e-spec-itwcw.ts
+++ packages/custom-webpack/examples/full-cycle-app/e2e/src/app.e2e-spec-itwcw.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('workspace-project App ITWCW', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- packages/custom-webpack/examples/full-cycle-app/e2e/src/app.e2e-spec-prod.ts
+++ packages/custom-webpack/examples/full-cycle-app/e2e/src/app.e2e-spec-prod.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('workspace-project App PROD', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
