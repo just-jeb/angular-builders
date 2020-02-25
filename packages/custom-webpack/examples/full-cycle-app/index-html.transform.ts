c34787521b6d60a2ea47ec245bee4aa2fa4b2f3f
vixnguyen <vinguyen.fitsgu@gmail.com>
2020-02-25 17:54:57 +0700

    Revert "using configuration instead of shorthand"
    This reverts commit b4f5ca423132fb81d3dd9f7bd6cda72c29677c8d.

--- packages/custom-webpack/examples/full-cycle-app/index-html.transform.ts
+++ packages/custom-webpack/examples/full-cycle-app/index-html.transform.ts
@@ -1,7 +1,6 @@
 export default (targetOptions, indexHtml) => {
   const i = indexHtml.indexOf('</body>');
   const config = `<p>Configuration: ${targetOptions.configuration}</p>`;
-  console.log(config);
   return `${indexHtml.slice(0, i)}
             ${config}
             ${indexHtml.slice(i)}`;
