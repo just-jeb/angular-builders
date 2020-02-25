bdb6a74afe9256a8d31e414f98c2f8b5e5385950
vixnguyen <vinguyen.fitsgu@gmail.com>
2020-02-25 17:52:29 +0700

    Revert "change content of index-html.transform.ts"
    This reverts commit 131c2b0bb743d5879d8229b309eee26b2aac1a9c.

--- packages/custom-webpack/examples/full-cycle-app/index-html.transform.ts
+++ packages/custom-webpack/examples/full-cycle-app/index-html.transform.ts
@@ -1,6 +1,7 @@
-module.exports = (targetOptions, indexHtml) => {
+export default (targetOptions, indexHtml) => {
   const i = indexHtml.indexOf('</body>');
   const config = `<p>Configuration: ${targetOptions.configuration}</p>`;
+  console.log(config);
   return `${indexHtml.slice(0, i)}
             ${config}
             ${indexHtml.slice(i)}`;
