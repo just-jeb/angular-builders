b4f5ca423132fb81d3dd9f7bd6cda72c29677c8d
vixnguyen <vinguyen.fitsgu@gmail.com>
2020-02-25 14:13:19 +0700

    using configuration instead of shorthand

--- packages/custom-webpack/examples/full-cycle-app/index-html.transform.ts
+++ packages/custom-webpack/examples/full-cycle-app/index-html.transform.ts
@@ -1,6 +1,7 @@
 export default (targetOptions, indexHtml) => {
   const i = indexHtml.indexOf('</body>');
   const config = `<p>Configuration: ${targetOptions.configuration}</p>`;
+  console.log(config);
   return `${indexHtml.slice(0, i)}
             ${config}
             ${indexHtml.slice(i)}`;
