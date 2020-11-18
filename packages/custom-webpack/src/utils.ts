54910e56ec00e8b12502cec161b56a34810721b8
Michel Hervé NGOUNOU <dev.ngounou@gmail.com>
2020-11-18 01:54:06 +0100

    chore: add tsconfig-paths for paths compiler-options

--- packages/custom-webpack/src/utils.ts
+++ packages/custom-webpack/src/utils.ts
@@ -12,5 +12,12 @@ export function tsNodeRegister(file: string = '', tsConfig?: string) {
         module: 'commonjs',
       },
     });
+
+    // Register paths in tsConfig
+    const tsconfigPaths = require('tsconfig-paths');
+    const { absoluteBaseUrl: baseUrl, paths } = tsconfigPaths.loadConfig(tsConfig);
+    if (baseUrl && paths) {
+      tsconfigPaths.register({ baseUrl, paths });
+    }
   }
 }
