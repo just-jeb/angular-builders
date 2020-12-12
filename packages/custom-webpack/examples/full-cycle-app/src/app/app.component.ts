1de68ee05b8138131d6536d56bbf5e43b7b977e8
Michel Hervé NGOUNOU <dev.ngounou@gmail.com>
2020-12-12 20:55:51 +0100

    feat: add test for the new feature

--- packages/custom-webpack/examples/full-cycle-app/src/app/app.component.ts
+++ packages/custom-webpack/examples/full-cycle-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare const APP_VERSION: string;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'full-cycle-app';
+  version = APP_VERSION;
 }
