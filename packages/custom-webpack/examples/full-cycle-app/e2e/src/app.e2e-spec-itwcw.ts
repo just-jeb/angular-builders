7ac76ed8ecabcbe5b5d33e27f4775c204189227b
Michel Hervé NGOUNOU <dev.ngounou@gmail.com>
2020-12-12 20:55:51 +0100

    feat: add test for the new feature

--- packages/custom-webpack/examples/full-cycle-app/e2e/src/app.e2e-spec-itwcw.ts
+++ packages/custom-webpack/examples/full-cycle-app/e2e/src/app.e2e-spec-itwcw.ts
@@ -1,3 +1,4 @@
+import { version } from '@project';
 import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
@@ -10,5 +11,10 @@ describe('workspace-project App', () => {
   it('should display paragraph coming from transform function', async () => {
     await page.navigateTo();
     expect(await page.getParagraphText()).toEqual('Configuration: itwcw');
+  });
+
+  it('should display version in div with `version` css class', async () => {
+    await page.navigateTo();
+    expect(await page.getVersionText()).toContain(version);
   });
 });
