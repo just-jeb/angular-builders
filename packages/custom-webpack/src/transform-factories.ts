03e5c28f630bb7c7ac2f8ebc77f0e06e5be4ee2f
JeB <jebbacca@gmail.com>
2021-02-03 10:51:35 +0200

    build: fix build

--- packages/custom-webpack/src/transform-factories.ts
+++ packages/custom-webpack/src/transform-factories.ts
@@ -1,6 +1,6 @@
 import { BuilderContext } from '@angular-devkit/architect';
 import { ExecutionTransformer } from '@angular-devkit/build-angular';
-import { IndexHtmlTransform } from '@angular-devkit/build-angular/src/utils/index-file/write-index-html';
+import { IndexHtmlTransform } from '@angular-devkit/build-angular/src/utils/index-file/index-html-generator';
 import { getSystemPath, normalize } from '@angular-devkit/core';
 import { Configuration } from 'webpack';
 import { CustomWebpackBuilder } from './custom-webpack-builder';
