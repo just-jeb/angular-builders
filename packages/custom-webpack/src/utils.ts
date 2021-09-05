877b5350ddc49dfdc16a9dcf5573138ccaa7e8e1
GerkinDev <alexandre.j.p.germain@gmail.com>
2021-09-05 21:05:52 +0200

    fix(custom-webpack): actual fix for #1031

--- packages/custom-webpack/src/utils.ts
+++ packages/custom-webpack/src/utils.ts
@@ -1,11 +1,16 @@
-/**
- * check for TS node registration
- * @param file: file name or file directory are allowed
- * @todo tsNodeRegistration: require ts-node if file extension is TypeScript
- */
-export function tsNodeRegister(file: string = '', tsConfig?: string) {
-  if (file && file.endsWith('.ts')) {
-    // Register TS compiler lazily
+const _tsNodeRegister = (() => {
+  let lastTsConfig: string | null | undefined;
+  return (tsConfig?: string) => {
+    // Check if the function was previously called with the same tsconfig
+    if (typeof lastTsConfig !== 'undefined' && (tsConfig ?? null) !== lastTsConfig) {
+      throw new Error('Called with multiple tsconfigs.');
+    }
+    if (lastTsConfig) {
+      return;
+    }
+    lastTsConfig = tsConfig ?? null;
+
+    // Register ts-node
     require('ts-node').register({
       project: tsConfig,
       compilerOptions: {
@@ -22,5 +27,17 @@ export function tsNodeRegister(file: string = '', tsConfig?: string) {
     if (baseUrl && paths) {
       tsconfigPaths.register({ baseUrl, paths });
     }
+  };
+})();
+
+/**
+ * check for TS node registration
+ * @param file: file name or file directory are allowed
+ * @todo tsNodeRegistration: require ts-node if file extension is TypeScript
+ */
+export function tsNodeRegister(file: string = '', tsConfig?: string) {
+  if (file && file.endsWith('.ts')) {
+    // Register TS compiler lazily
+    _tsNodeRegister(tsConfig);
   }
 }
