1f20ebae0c26327b098573594a3da7d4ebcfc17a
JeB <jebbacca@gmail.com>
2021-11-04 12:33:36 +0200

    ci: fix update:examples script

--- scripts/update-example.js
+++ scripts/update-example.js
@@ -3,8 +3,12 @@ const package = require(`${process.cwd()}/package.json`);
 const version = Number.parseInt(process.argv.slice(2));
 
 const updateAngularVersion = () => {
+  console.log(`Updating Angular version for ${package.name}`);
   execSync(`npx @angular/cli@${version} update @angular/core@${version} @angular/cli@${version}`);
+  console.log(`Successfully updated ${package.name} to Angular ${version}`);
+  console.log('Committing the changes');
   execSync(`git commit -am 'chore(deps): update ${package.name} to Angular ${version}'`);
+  console.log('Successfully committed the changes');
 };
 
 updateAngularVersion();
