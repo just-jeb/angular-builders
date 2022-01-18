dd6695bf0d7e8896f4f4850b809c30eb9196089a
JeB <jebbacca@gmail.com>
2022-01-18 16:04:46 +0200

    chore: another fix

--- packages/jest/scripts/ci.sh
+++ packages/jest/scripts/ci.sh
@@ -77,7 +77,7 @@ multiAppTestOptions=(
     "yarn test my-first-app|--testNamePattern=^AppComponent should create the app$|1|1|1|3|2||"
     "yarn test my-shared-library|--testPathPattern=src/lib/my-shared-library.service.spec.ts$|1|1|1|1|||"
     "yarn test my-shared-library|--testPathPattern=src/lib/my-shared-library.component.spec.ts$;--testPathPattern=src/lib/my-shared-library.service.spec.ts$|2|2|2|2|||"
-    "yarn test my-shared-library|--find-related-tests projects/my-shared-library/src/lib/my-shared-library.service.ts,projects/my-shared-library/src/lib/my-shared-library.component.ts|2|2|2|2|||"
+    "yarn test my-shared-library|--find-related-tests;src/lib/my-shared-library.service.ts,src/lib/my-shared-library.component.ts|2|2|2|2|||"
     "yarn test my-shared-library|--rootDir=`pwd`/examples/multiple-apps/projects/my-shared-library|2|2|2|2|||"
 )
 (ciApp ../../examples/jest/simple-app --protractor-config=./e2e/protractor-ci.conf.js simpleAppTestOptions)
