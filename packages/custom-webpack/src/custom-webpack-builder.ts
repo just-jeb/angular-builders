b5df142f4dab948b0feb8a4eea4b8954ae3a8d9b
JeB <jebbacca@gmail.com>
2022-01-25 13:49:23 +0200

    feat(custom-webpack): support esm

--- packages/custom-webpack/src/custom-webpack-builder.ts
+++ packages/custom-webpack/src/custom-webpack-builder.ts
@@ -6,6 +6,9 @@ import { CustomWebpackBuilderConfig } from './custom-webpack-builder-config';
 import { tsNodeRegister } from './utils';
 import { TargetOptions } from './type-definition';
 import { CustomWebpackBrowserSchema } from './browser';
+import { pathToFileURL } from 'url';
+import { extname } from 'path';
+import { existsSync } from 'fs';
 
 export const defaultWebpackConfigPath = 'webpack.config.js';
 
@@ -39,7 +42,7 @@ export class CustomWebpackBuilder {
     const webpackConfigPath = config.path || defaultWebpackConfigPath;
     const path = `${getSystemPath(root)}/${webpackConfigPath}`;
     const tsConfig = `${getSystemPath(root)}/${buildOptions.tsConfig}`;
-    const configOrFactoryOrPromise = resolveCustomWebpackConfig(path, tsConfig, logger);
+    const configOrFactoryOrPromise = await resolveCustomWebpackConfig(path, tsConfig, logger);
 
     if (typeof configOrFactoryOrPromise === 'function') {
       // That exported function can be synchronous either
@@ -65,18 +68,63 @@ export class CustomWebpackBuilder {
   }
 }
 
-function resolveCustomWebpackConfig(
+async function resolveCustomWebpackConfig(
   path: string,
   tsConfig: string,
   logger: logging.LoggerApi
-): CustomWebpackConfig {
+): Promise<CustomWebpackConfig> {
   tsNodeRegister(path, tsConfig, logger);
 
-  const customWebpackConfig = require(path);
+  const customWebpackConfig = await getWebpackConfig(path);
   // If the user provides a configuration in TS file
   // then there are 2 cases for exporing an object. The first one is:
   // `module.exports = { ... }`. And the second one is:
   // `export default { ... }`. The ESM format is compiled into:
   // `{ default: { ... } }`
-  return customWebpackConfig.default || customWebpackConfig;
+  return (customWebpackConfig as any).default || customWebpackConfig;
+}
+
+/**
+ * This uses a dynamic import to load a module which may be ESM.
+ * CommonJS code can load ESM code via a dynamic import. Unfortunately, TypeScript
+ * will currently, unconditionally downlevel dynamic import into a require call.
+ * require calls cannot load ESM code and will result in a runtime error. To workaround
+ * this, a Function constructor is used to prevent TypeScript from changing the dynamic import.
+ * Once TypeScript provides support for keeping the dynamic import this workaround can
+ * be dropped.
+ *
+ * @param modulePath The path of the module to load.
+ * @returns A Promise that resolves to the dynamically imported module.
+ */
+function loadEsmModule<T>(modulePath: string | URL): Promise<T> {
+  return new Function('modulePath', `return import(modulePath);`)(modulePath) as Promise<T>;
+}
+
+export async function getWebpackConfig(configPath: string): Promise<CustomWebpackConfig> {
+  switch (extname(configPath)) {
+    case '.mjs':
+      // Load the ESM configuration file using the TypeScript dynamic import workaround.
+      // Once TypeScript provides support for keeping the dynamic import this workaround can be
+      // changed to a direct dynamic import.
+      return (await loadEsmModule<{ default: CustomWebpackConfig }>(pathToFileURL(configPath)))
+        .default;
+    case '.cjs':
+      return require(configPath);
+    default:
+      // The file could be either CommonJS or ESM.
+      // CommonJS is tried first then ESM if loading fails.
+      try {
+        return require(configPath);
+      } catch (e: any) {
+        if (e.code === 'ERR_REQUIRE_ESM') {
+          // Load the ESM configuration file using the TypeScript dynamic import workaround.
+          // Once TypeScript provides support for keeping the dynamic import this workaround can be
+          // changed to a direct dynamic import.
+          return (await loadEsmModule<{ default: CustomWebpackConfig }>(pathToFileURL(configPath)))
+            .default;
+        }
+
+        throw e;
+      }
+  }
 }
