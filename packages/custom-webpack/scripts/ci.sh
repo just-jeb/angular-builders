dceaeb0b984f59075a232402028f330bd4ab38b4
JeB <jebbacca@gmail.com>
2022-01-28 15:29:10 +0200

    ci: add all permutations to CI run

--- packages/custom-webpack/scripts/ci.sh
+++ packages/custom-webpack/scripts/ci.sh
@@ -2,13 +2,19 @@
 
 #Check that it works the same way angular-devkit builders do when there is no custom webpack config provided
 cd ../../examples/custom-webpack/sanity-app
-#ng build is verified during lerna run
 yarn test --browsers=ChromeHeadlessCI
-yarn e2e
+yarn e2e # no custom config at all
+yarn e2e -c esm # cjs custom config
+yarn e2e -c cjs # esm custom config
+
+cd ../sanity-app-esm
+yarn test --browsers=ChromeHeadlessCI
+yarn e2e # no custom config at all
+yarn e2e -c esm # cjs custom config
+yarn e2e -c cjs # esm custom config
 
 #Check scenarios with custom webpack config
 cd ../full-cycle-app
-#ng build is verified during lerna run
 #check karma builder
 yarn test --browsers=ChromeHeadlessCI
 #check browser and dev-server
