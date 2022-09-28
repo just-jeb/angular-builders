8bb4ac35fb0d6cb7ff51d88ee890c7e55278c7b3
JeB <work@justjeb.com>
2022-09-28 12:41:34 +0300

    fix: cypress configs

---
+++ examples/custom-webpack/full-cycle-app/cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  video: false,
+  screenshotOnRunFailure: false,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    specPattern: 'e2e/src/**/*.e2e-spec.ts',
+    baseUrl: 'http://localhost:5000',
+    supportFile: false,
+  },
+});
