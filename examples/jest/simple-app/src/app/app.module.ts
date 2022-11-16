7009ff62de1b32391b8c737546921cdab94a06f3
Lionel Meunier <lionel.meunier@agicap.com>
2022-11-16 15:52:30 +0100

    test(jest): add test shard 1/2 and 2/2 and suites skipped

--- examples/jest/simple-app/src/app/app.module.ts
+++ examples/jest/simple-app/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LinkComponent } from './link/link.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LinkComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ examples/jest/simple-app/src/app/link/link.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LinkComponent } from './link.component';
+
+describe('LinkComponent', () => {
+  let component: LinkComponent;
+  let fixture: ComponentFixture<LinkComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LinkComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LinkComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create 3 links', () => {
+    expect(fixture.debugElement.nativeElement.querySelectorAll('a').length).toEqual(3);
+  });
+});
