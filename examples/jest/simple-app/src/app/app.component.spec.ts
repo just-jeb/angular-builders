193dbb750bc861933301ace4e48c8a4853974e1a
lme-agicap <116797118+lme-agicap@users.noreply.github.com>
2022-11-17 10:46:42 +0100

    test(jest): add test shard 1/2 and 2/2 and suites skipped (#1317)

--- examples/jest/simple-app/src/app/app.component.spec.ts
+++ examples/jest/simple-app/src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
         declarations: [AppComponent],
+        schemas: [
+          CUSTOM_ELEMENTS_SCHEMA
+        ]
       }).compileComponents();
     })
   );
