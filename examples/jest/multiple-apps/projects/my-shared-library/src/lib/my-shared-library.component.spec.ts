c26fe9e62a21dd42b14e9bb2e622259d013aba44
JeB <work@justjeb.com>
2024-05-26 15:20:02 +0300

    fix: tests

--- examples/jest/multiple-apps/projects/my-shared-library/src/lib/my-shared-library.component.spec.ts
+++ examples/jest/multiple-apps/projects/my-shared-library/src/lib/my-shared-library.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MySharedLibraryComponent } from './my-shared-library.component';
 
@@ -6,7 +6,7 @@ describe('MySharedLibraryComponent', () => {
   let component: MySharedLibraryComponent;
   let fixture: ComponentFixture<MySharedLibraryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [MySharedLibraryComponent],
     }).compileComponents();
