c49717953a8241758f39b97936819273737127fb
Jeb <work@justjeb.com>
2025-06-18 15:58:12 +0200

    fix: remove forceEsbuild property and clean up dependencies - Remove forceEsbuild property from custom-esbuild examples (no longer valid in @angular/build schema) - Remove redundant @angular/build resolution from root package.json - Update scheme comments to reflect correct package versions

--- packages/custom-webpack/src/schemes.ts
+++ packages/custom-webpack/src/schemes.ts
@@ -1,4 +1,4 @@
-// Base schemes from build-angular@19.2.0
+// Base schemes from @angular-devkit/build-angular@20.0.0
 module.exports = [
   {
     originalSchemaPath: '@angular-devkit/build-angular/src/builders/browser/schema.json',
