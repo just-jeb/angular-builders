f23d5147240a2aeef4a33469cdc1b8db86847eb2
Jeb <work@justjeb.com>
2025-12-14 12:13:34 +0100

    @angular/core migration - bootstrap-options-migration
    Migrates deprecated bootstrap options to providers.

--- examples/custom-esbuild/sanity-esbuild-app-esm/src/main.ts
+++ examples/custom-esbuild/sanity-esbuild-app-esm/src/main.ts
@@ -1,6 +1,7 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig);
+bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]});
